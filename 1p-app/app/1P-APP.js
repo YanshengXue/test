472ecf9937dac2db3bb730b4b5664bfc124d2543
suman <suman.jupudi@thomsonreuters.com>
2015-03-10 23:05:57 -0700

    1P-APP - UI APP project template

---
+++ 1p-app/app/1P-APP.js
@@ -0,0 +1,21 @@
+(function() {
+var onepApp = angular.module('1P-APP',[]);
+onepApp.controller('oneAppCtlr',['$scope', '$http', function($scope, $http){
+    $scope.method = 'GET';
+  //Change the Service URL as required - Going through Zuul requires additional config work.
+    $scope.url = 'http://localhost:7001/hello';
+    $scope.code = null;
+    $scope.response = null;
+    $http({method: $scope.method, url: $scope.url}).
+        success(function(data, status) {
+            $scope.status = status;
+            $scope.data = data;
+        }).
+        error(function(data, status) {
+            $scope.data = data || "Request failed";
+            $scope.status = status;
+        });
+
+}]);
+
+})();
