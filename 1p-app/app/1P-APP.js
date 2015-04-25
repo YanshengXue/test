89c7ff0e38c5cb60b2e1e5cd325e07e77e249426
edsalat <edward.salatovka@thomsonreuters.com>
2015-04-24 17:37:28 -0700

    Zuul demo modificatins

--- 1p-app/app/1P-APP.js
+++ 1p-app/app/1P-APP.js
@@ -3,7 +3,8 @@ var onepApp = angular.module('1P-APP',[]);
 onepApp.controller('oneAppCtlr',['$scope', '$http', function($scope, $http){
     $scope.method = 'GET';
   //Change the Service URL as required - Going through Zuul requires additional config work.
-    $scope.url = 'http://localhost:7001/hello';
+    //$scope.url = 'http://localhost:7001/hello';
+    $scope.url = '/api/hello';
     $scope.code = null;
     $scope.response = null;
     $http({method: $scope.method, url: $scope.url}).
